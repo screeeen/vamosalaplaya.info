a1cc99d7ac15003bbf41646b85e3339bc62be417
Miguel Alvarez <miguel@screenimplosion.com>
2026-06-15 12:33:20 +0200

    Updates

--- assets/main-DuZ_0NVY.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t,r,A as o}from"./App-Bplnkrpx.js";e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
---
+++ assets/themorethemerrier-CRmQUN9H.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,r,A as o}from"./App-D9SioqNz.js";e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{hideStayTravel:!0})}));
